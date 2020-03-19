a359977ab9003f77ec706f18b61bedc94e72e565
Hariririski <hariririski@gmail.com>
2020-03-19 18:30:45 +0700

    menu

--- application/views/share/Menu_pcside2.php
+++ application/views/share/Menu_pcside2.php
@@ -2,12 +2,20 @@
 <?php ini_set('display_errors','off');?>
 <aside class="sidenav-main nav-expanded nav-lock nav-collapsible sidenav-light sidenav-active-square">
   <div class="brand-sidebar">
-    <a class="brand-logo darken-1" href="<?php echo site_url(); ?>Pc">
-      <img class="hide-on-med-and-down" src="<?php echo site_url(); ?>assets/config/atrbpn.png" alt="materialize logo"/>
-      <img class="show-on-medium-and-down hide-on-med-and-up" src="<?php echo site_url(); ?>assets/pc/images/logo/materialize-logo.png" alt="materialize logo"/>
-      <span class="logo-text hide-on-med-and-down">Pengaduan</span>
+    <h1 class="logo-wrapper">
+      <a class="brand-logo darken-1" href="<?php echo site_url(); ?>Pc">
+        <img class="hide-on-med-and-down" src="<?php echo site_url(); ?>assets/config/atrbpn.png" alt="materialize logo"/>
+        <img class="show-on-medium-and-down hide-on-med-and-up" src="<?php echo site_url(); ?>assets/pc/images/logo/materialize-logo.png" alt="materialize logo"/>
+        <span class="logo-text hide-on-med-and-down">Pengaduan</span>
+      </a>
+      <a class="navbar-toggler" href="#">
+        <i class="material-icons">radio_button_checked</i>
+      </a>
+  </h1>
   </div>
   <ul class="sidenav sidenav-collapsible leftside-navigation collapsible sidenav-fixed menu-shadow" id="slide-out" data-menu="menu-navigation" data-collapsible="menu-accordion">
+
+
     <li class="navigation-header"><a class="navigation-header-text">Menu</a><i class="navigation-header-icon material-icons">more_horiz</i>
     </li>
     <li class="bold"><a class="waves-effect waves-cyan " href="<?php echo site_url(); ?>Pc"><i class="material-icons">mail_outline</i><span class="menu-title" data-i18n="Mail">Home</span></a>
@@ -45,6 +53,5 @@
 
     <li class="bold"><a class="waves-effect waves-cyan " href="<?php echo site_url(); ?>Login"><i class="material-icons">check</i><span class="menu-title" data-i18n="ToDo">Login</span></a>
     </li>
-  </ul>
-  <div class="navigation-background"></div><a class="sidenav-trigger btn-sidenav-toggle btn-floating btn-medium waves-effect waves-light hide-on-large-only" href="#" data-target="slide-out"><i class="material-icons">menu</i></a>
-</aside>
+      <div class="navigation-background"></div><a class="sidenav-trigger btn-sidenav-toggle btn-floating btn-medium waves-effect waves-light hide-on-large-only" href="#" data-target="slide-out"><i class="material-icons">menu</i></a>
+    </aside>
